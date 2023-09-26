54c086d7422f358b035f981dfc626c1fcdd56d99
Campbell Lowman <campbelllowman@gmail.com>
2023-09-26 16:44:29 -0400

    tweak to mlb team updater

--- functions/update_mlb_schedules/populate_mlb_teams.ts
+++ functions/update_mlb_schedules/populate_mlb_teams.ts
@@ -5,6 +5,7 @@ import { Database } from '../database.types.ts'
 const supabase = createClient<Database>('http://localhost:54321', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU')
 
 mlbTeamData.sports[0].leagues[0].teams.forEach(async element => {
+    console.log(element.team.id)
     console.log(element.team.shortDisplayName)
     console.log(element.team.displayName)
     const logo = element.team.logos.filter((logo) => {if(logo.rel.includes('default')) return logo })
@@ -12,6 +13,7 @@ mlbTeamData.sports[0].leagues[0].teams.forEach(async element => {
     console.log(element.team.abbreviation)
 
     const { error } = await supabase.from("mlb_teams").insert({
+        id: element.team.id,
         short_display_name: element.team.shortDisplayName,
         display_name: element.team.displayName,
         logo: logo[0].href,
